33696cc4e1f97c43bc683bc33f1064353e1b30b3
zhejian <jianzher@sina.com>
2019-03-17 23:43:13 -0700

    pathsumii

---
+++ .history/113.path-sum-ii_20190317233446.java
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=113 lang=java
+ *
+ * [113] Path Sum II
+ *
+ * https://leetcode.com/problems/path-sum-ii/description/
+ *
+ * algorithms
+ * Medium (39.22%)
+ * Total Accepted:    216.7K
+ * Total Submissions: 546.5K
+ * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,5,1]\n22'
+ *
+ * Given a binary tree and a sum, find all root-to-leaf paths where each path's
+ * sum equals the given sum.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * Given the below binary tree and sum = 22,
+ * 
+ * 
+ * ⁠     5
+ * ⁠    / \
+ * ⁠   4   8
+ * ⁠  /   / \
+ * ⁠ 11  13  4
+ * ⁠/  \    / \
+ * 7    2  5   1
+ * 
+ * 
+ * Return:
+ * 
+ * 
+ * [
+ * ⁠  [5,4,11,2],
+ * ⁠  [5,8,4,5]
+ * ]
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        
+    }
+}
+
---
+++ 113.path-sum-ii.java
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=113 lang=java
+ *
+ * [113] Path Sum II
+ *
+ * https://leetcode.com/problems/path-sum-ii/description/
+ *
+ * algorithms
+ * Medium (39.22%)
+ * Total Accepted:    216.7K
+ * Total Submissions: 546.5K
+ * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,5,1]\n22'
+ *
+ * Given a binary tree and a sum, find all root-to-leaf paths where each path's
+ * sum equals the given sum.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * Given the below binary tree and sum = 22,
+ * 
+ * 
+ * ⁠     5
+ * ⁠    / \
+ * ⁠   4   8
+ * ⁠  /   / \
+ * ⁠ 11  13  4
+ * ⁠/  \    / \
+ * 7    2  5   1
+ * 
+ * 
+ * Return:
+ * 
+ * 
+ * [
+ * ⁠  [5,4,11,2],
+ * ⁠  [5,8,4,5]
+ * ]
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
+        dfs(root, new ArrayList<>(), res,  sum);
+        return res;
+    }
+    private void dfs(TreeNode root, List<Integer> path, List<List<Integer>> res, int sum) {
+        path.add(root.val);
+        if(root.left == null && root.right == null && root.val == sum) {
+            res.add(new ArrayList<>(path));
+        }
+        if(root.left != null) {
+            dfs(root.left, path, res, sum - root.val);
+        }
+        if(root.right != null) {
+            dfs(root.right, path, res, sum - root.val);
+        }
+        path.remove(path.size() - 1);
+    }
+}
+
